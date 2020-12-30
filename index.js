c93640ab7a56538bc2fd6e5280ca09f12524796e
Devyash <bordia999@gmail.com>
2020-12-30 14:53:40 +0530

    added cors for deployment

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const fs = require("fs");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
+app.use(cors());
 
 app.post("/addNames", (req, res) => {
   const name = req.body["name"];
